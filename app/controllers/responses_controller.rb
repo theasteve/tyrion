0c67f1729d632a9323d8aa8ae81d3d2aea25b604
theasteve <stevenaguilarcs@gmail.com>
2020-11-05 16:19:31 -0500

    Remove use of instance variables in controller

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,40 +1,40 @@
 class ResponsesController < ApplicationController
   def index
-    @responses = Response.all
-    render json: ResponseSerializer.new(@response).serializable_hash, status: 200
+    responses = Response.all
+    render json: ResponseSerializer.new(responses).serializable_hash, status: 200
   end
 
   def show
-    @response = Response.find(params[:id])
+    response = Response.find(params[:id])
 
-    render json: ResponseSerializer.new(@response).serializable_hash, status: 200
+    render json: ResponseSerializer.new(response).serializable_hash, status: 200
   end
 
   def create
-    @response = Response.new(response_params)
-    @response.user_id = 1
-    @response.topic_id = 1
+    response = Response.new(response_params)
+    response.user_id = 1
+    response.topic_id = 1
 
-    if @response.save
-      render json: ResponseSerializer.new(@response).serializable_hash, status: 201
+    if response.save
+      render json: ResponseSerializer.new(response).serializable_hash, status: 201
     else
-      render json: @response.errors.details
+      render json: response.errors.details
     end
   end
 
   def update
-    @response = Response.find(params[:id])
+    response = Response.find(params[:id])
 
-    if @response.update(response_params)
-      render json: ResponseSerializer.new(@response).serializable_hash, status: 200
+    if response.update(response_params)
+      render json: ResponseSerializer.new(response).serializable_hash, status: 200
     else
-      render json: @response.errors.details
+      render json: response.errors.details
     end
   end
 
   def destroy
-    @response = Response.find(params[:id])
-    @response.destroy
+    response = Response.find(params[:id])
+    response.destroy
 
     head :no_content
   end
