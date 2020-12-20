23d6e3cf62acfeb475400681472f96304d12a3b7
STEVEN AGUILAR <stevenaguilarcs@gmail.com>
2020-12-20 13:11:51 -0500

    Add boolean type for StockTransaction table (#16)
    * Add boolean type for StockTransaction table
    
    * Remove reference migrations

--- db/migrate/20201217163745_create_stock_transactions.rb
+++ db/migrate/20201217163745_create_stock_transactions.rb
@@ -1,10 +1,12 @@
 class CreateStockTransactions < ActiveRecord::Migration[6.0]
   def change
     create_table :stock_transactions do |t|
-      t.string :followers
-      t.string :holders
-      t.string :buyers
-      t.string :sellers
+      t.boolean :following
+      t.boolean :holding
+      t.boolean :buying
+      t.boolean :selling
+      t.references :user, null: false, foreign_key: true
+      t.references :stock, null: false, foreign_key: true
 
       t.timestamps
     end
