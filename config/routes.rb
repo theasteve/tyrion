0ebd6b48a2bb6cb60218b9689ee32e0ec0f3dc03
STEVEN AGUILAR <stevenaguilarcs@gmail.com>
2021-01-02 16:12:15 -0500

    Handle PATCH request on stock controller (#26)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :index]
   resources :sessions, only: [:create, :delete]
-  resources :stocks, only: [:index, :show]
+  resources :stocks, only: [:index, :show, :update]
   resources :spacs, only: [:index, :show]
 
   get :logout, to: "sessions#logout"
