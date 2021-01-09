e8b88f92b886df732f0fc55afb65004d47275b8b
theasteve <stevenaguilarcs@gmail.com>
2021-01-09 15:49:53 -0500

    Deployment test to EC2 instance

--- app/controllers/westeros_controller.rb
+++ app/controllers/westeros_controller.rb
@@ -1,7 +1,7 @@
-class WesterosController < ApplicationController
-  include ReverseProxy::Controller
-
-  def index
-    reverse_proxy "http://localhost:4200"
-  end
-end
+# class WesterosController < ApplicationController
+#   include ReverseProxy::Controller
+#
+#   def index
+#     reverse_proxy "http://localhost:4200"
+#   end
+# end
