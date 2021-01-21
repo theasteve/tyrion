68c551e7545e6d6592df36dae3a3b0341fc90c7c
STEVEN AGUILAR <stevenaguilarcs@gmail.com>
2021-01-21 08:54:55 -0500

    Redirect root path to top stocks page (#35)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'westeros#index'
+  get '/westeros', to: 'westeros#index'
+
   resources :users, only: [:create, :index]
   resources :sessions, only: [:create, :delete]
   resources :stocks, only: [:index, :show, :update]
