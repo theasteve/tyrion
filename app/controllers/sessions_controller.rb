dc35b3379d77805d4dac272047647aea6e275b09
theasteve <stevenaguilarcs@gmail.com>
2021-02-01 12:39:28 -0500

    Move mixpanel implementation to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(session_params)
 
     if user
+      track_user_on_mixpanel(user)
       session[:user_id] = user.id
       cookies[:logged_in] = { value: true, expires: 30.days }
       redirect_to "/westeros/top-stocks"
@@ -30,5 +31,20 @@ class SessionsController < ApplicationController
     params.name = auth.dig(:info, :name)
     params.image_url = auth.dig(:extra, :raw_info, :subreddit, :icon_img).split('?').first
     params = ActionController::Parameters.new(params).permit!
+  end
+
+  private
+
+  def track_user_on_mixpanel(user)
+    track_params = mixpanel_user_params(user)
+    tracker.people.set(user.id, track_params)
+  end
+
+  def mixpanel_user_params(user)
+    {
+      USER_ID: user.id,
+      name: user.name,
+      sign_up: DateTime.now
+    }
   end
 end
