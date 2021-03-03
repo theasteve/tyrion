045063262709e5a5ad3fa7871aaf872cf4c99ea2
MansurTsutiev <mtsutiev@movableink.com>
2021-03-02 21:08:18 -0500

    feat: activate specific revision based on param

--- app/controllers/westeros_controller.rb
+++ app/controllers/westeros_controller.rb
@@ -7,7 +7,9 @@ class WesterosController < ActionController::Base
 
   def index
     if Rails.env.production?
-      url_response = URI.open(ENV['S3_EMBER_APP']).read
+      s3_url = ENV['S3_EMBER_APP'].dup
+      s3_url << ":#{params[:revision]}" if params[:revision]
+      url_response = URI.open(s3_url).read
       render html: url_response.html_safe
     else
       reverse_proxy 'http://localhost:4200'
